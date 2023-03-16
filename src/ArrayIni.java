a3516857a4411bc7a404e0295e30add0533f2aa3
YayCover9000 <yassuanf@googlemail.com>
2023-03-16 19:12:27 +0100

    add ziffer to String

--- src/ArrayIni.java
+++ src/ArrayIni.java
@@ -1,2 +1,31 @@
 public class ArrayIni {
+    public static void main (String [] args) {
+        int ziffer = 7;
+        int [] Array = new int[5];
+        ArrayIni.ini(Array, ziffer);
+    }
+    public static void ini (int[] ArrayFirst, int ziffer) {
+        String zwischen = Integer.toString(ziffer);
+        String zifferString = Integer.toString(ziffer);
+        System.out.println("Ziffer To String: " + zifferString);
+        for (int k = 0; k < ArrayFirst.length; k++) {
+            zwischen += zifferString;
+            System.out.println("Zwischen: " + zwischen);
+        }
+
+        if (ziffer > 0 && ziffer < 10) {
+            for (int i = 0; i < ArrayFirst.length; i++) {
+                ArrayFirst[i] = ziffer;
+                for(int j = 0; j < ArrayFirst[i]; j++) {
+
+                }
+            }
+            for (int e : ArrayFirst) {
+                System.out.println("Ziffer: " + e);
+            }
+        } else {
+            System.out.println("LEER");
+        }
+
+    }
 }
